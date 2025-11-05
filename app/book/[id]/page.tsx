1df777c0bf7aa0abefb446d174d8151451686ef1
Sukanya Ratanotayanon <89356791+sratanota@users.noreply.github.com>
2025-11-05 15:27:31 +0700

    Finish front and bookdetail no image

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { getBookById } from '../../../lib/books';
-
+import { createClient } from "../../../lib/supabase/server";
+import { cookies } from "next/headers";
 
 type Props = { params: { id: string } };
 
 
-export default function BookPage({ params }: Props) {
-const book = getBookById(params.id);
+export default async function BookPage({ params }: Props) {
+
+    const cookieStore = await cookies();
+    const supabase = createClient(cookieStore);
+
+
+    const {data: book, error:error1 } = await supabase. from ('book')
+        .select(`*, author(name)`)
+        .eq('isbn', params.id);
+
+    if (error1){
+        console.log("Error fetching  info: ", error1);
+    }
+    console.log (book);
+
 if (!book) return <div className="container py-12">Book not found</div>;
+
+const aBook = book[0];
 return (
 <section className="container py-12 grid md:grid-cols-3 gap-8">
-<img src={book.cover} alt={book.title} className="w-full h-auto" />
+<img src={aBook.cover} alt={aBook.title} className="w-full h-auto" />
 <div className="md:col-span-2">
-<h1 className="text-3xl font-bold">{book.title}</h1>
-<p className="text-gray-700 mt-4">{book.description}</p>
+<h1 className="text-3xl font-bold">{aBook.title}</h1>
+
+<p className="text-gray-700 mt-4">{aBook.publisher}</p>
+<p className="text-gray-700 mt-4">{aBook.pubyear}</p>
+
 </div>
 </section>
 );
--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,32 @@ export default async function Home() {
     const cookieStore = await cookies();
     const supabase = createClient(cookieStore);
 
-    const {data: newestBooks, error } = await supabase. from ('bookcopy')
-        .select(`copyid,acquisitiondate , book ( isbn, title, publisher, pubyear, author (name))`)
-        .order('acquisitiondate', {ascending:false})
-        .limit(5);
-    if (error){
-        console.log("Error fetching newest book: ", error);
+
+    const {data: newestBooks, error:error1 } = await supabase. from ('newest_bookcopies')
+        .select(`*`);
+
+    if (error1){
+        console.log("Error fetching newest book: ", error1);
     }
+
+    const {data: popBook, error:error2} = await supabase.from('borrowstats')
+    .select(`isbn, borrowcount, book(title, author(name))`)
+    .order("borrowcount", {ascending:false})
+    .limit(6);
+    if (error2){
+        console.log("Error fetching newest book: ", error2);
+    }
+    const popularBooks = popBook.map((b)=>{
+        let book:any = b.book;
+        let names = book.author.map(a=>a.name).join(", ");
+        return {isbn:b.isbn, title:book.title, authors:names};
+    });
+
 return (
 <>
 <Hero />
 <FeaturedBooks books={newestBooks} sectionName="New Books" />
-
+<FeaturedBooks books={popularBooks} sectionName="Popular Books" />
 </>
 );
 }
